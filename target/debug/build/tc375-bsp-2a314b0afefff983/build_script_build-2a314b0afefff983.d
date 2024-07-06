b5b83da1d19d94a63d758f11790dd7fbe7a2e805
Jeff <jefferson.janisch@gmail.com>
2024-07-06 19:02:44 +0200

    test commit from new vm

--- target/debug/build/tc375-bsp-2a314b0afefff983/build_script_build-2a314b0afefff983.d
+++ target/debug/build/tc375-bsp-2a314b0afefff983/build_script_build-2a314b0afefff983.d
@@ -1,5 +1,5 @@
-C:\Users\jeffe\Desktop\Project\tc375-freertos\tc375-freertos\target\debug\build\tc375-bsp-2a314b0afefff983\build_script_build-2a314b0afefff983.exe: build.rs
+C:\Users\jeffe\Desktop\project\tc375-freertos\target\debug\build\tc375-bsp-2a314b0afefff983\build_script_build-2a314b0afefff983.exe: build.rs
 
-C:\Users\jeffe\Desktop\Project\tc375-freertos\tc375-freertos\target\debug\build\tc375-bsp-2a314b0afefff983\build_script_build-2a314b0afefff983.d: build.rs
+C:\Users\jeffe\Desktop\project\tc375-freertos\target\debug\build\tc375-bsp-2a314b0afefff983\build_script_build-2a314b0afefff983.d: build.rs
 
 build.rs:
--- target/debug/build/tc3xx-service-9b2dccda3bc988a3/build_script_build-9b2dccda3bc988a3.d
+++ target/debug/build/tc3xx-service-9b2dccda3bc988a3/build_script_build-9b2dccda3bc988a3.d
@@ -1,6 +1,6 @@
-C:\Users\jeffe\Desktop\Project\tc375-freertos\tc375-freertos\target\debug\build\tc3xx-service-9b2dccda3bc988a3\build_script_build-9b2dccda3bc988a3.exe: tc37x-hal\tc3xx-service\build.rs tc37x-hal\tc3xx-service\ld/tc37xA_memory.ld tc37x-hal\tc3xx-service\ld/tc37x_bsp_example_llvm.ld
+C:\Users\jeffe\Desktop\project\tc375-freertos\target\debug\build\tc3xx-service-9b2dccda3bc988a3\build_script_build-9b2dccda3bc988a3.exe: tc37x-hal\tc3xx-service\build.rs tc37x-hal\tc3xx-service\ld/tc37xA_memory.ld tc37x-hal\tc3xx-service\ld/tc37x_bsp_example_llvm.ld
 
-C:\Users\jeffe\Desktop\Project\tc375-freertos\tc375-freertos\target\debug\build\tc3xx-service-9b2dccda3bc988a3\build_script_build-9b2dccda3bc988a3.d: tc37x-hal\tc3xx-service\build.rs tc37x-hal\tc3xx-service\ld/tc37xA_memory.ld tc37x-hal\tc3xx-service\ld/tc37x_bsp_example_llvm.ld
+C:\Users\jeffe\Desktop\project\tc375-freertos\target\debug\build\tc3xx-service-9b2dccda3bc988a3\build_script_build-9b2dccda3bc988a3.d: tc37x-hal\tc3xx-service\build.rs tc37x-hal\tc3xx-service\ld/tc37xA_memory.ld tc37x-hal\tc3xx-service\ld/tc37x_bsp_example_llvm.ld
 
 tc37x-hal\tc3xx-service\build.rs:
 tc37x-hal\tc3xx-service\ld/tc37xA_memory.ld:
